fce5117e9c790b219819b5f44b87ac2eb65ff7fe
Ale <marialecano01@gmail.com>
2021-10-06 17:35:51 -0300

    minor issues fixed

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
   }
 
   return (
-    <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>;
       <main>
         <div className='search-box'>
           <input className='search-bar'
